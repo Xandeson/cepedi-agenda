9b59e91defc238bfe48eb72559f1a2d04f7d119c
Gabriella-Aquino <gabyyaquino8@gmail.com>
2024-01-11 21:20:02 -0300

    primeiros passos

---
+++ appTask/src/components/Task/styles.ts
@@ -0,0 +1,8 @@
+import { View } from "react-native";
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+    width: 100%;
+    height: 40px;
+    background-color: aliceblue;
+`;
